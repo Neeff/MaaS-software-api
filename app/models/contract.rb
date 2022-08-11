28a1bac1a3121129be16425e1b05b28f4faa635f
Neeff <dpincheira2016@alu.uct.cl>
2022-08-11 17:39:20 -0400

    Add test to model contract

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,3 +1,10 @@
 class Contract < ApplicationRecord
+  validates :business_days,
+            :init_hour,
+            :finish_hour,
+            :init_weekend_hour,
+            :finish_weekend_hour,
+            presence: true
+
   belongs_to :service
 end
