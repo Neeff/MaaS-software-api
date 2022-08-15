961fa68b193489d48d60ab61b669ae9e60748b16
Neeff <dpincheira2016@alu.uct.cl>
2022-08-14 22:44:35 -0400

    Add params to get routes
    Refer:
      - Task: modify get routes to accept service_id parameter
    
    Why i'm adding these lines of code
      - add these lines of code to pass the service id as a parameter to the get routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :available_hours, only: %i[index update], defaults: { format: :json }
+  get 'available_hours/:service_id', to: 'available_hours#index', defaults: { format: :json }
+  get 'shifts/:service_id', to: 'shifts#index', defaults: { format: :json }
+  resources :available_hours, only: %i[update], defaults: { format: :json }
   resources :services, defaults: { format: :json }
-  resources :shifts, only: %i[index update], defaults: { format: :json }
+  resources :shifts, only: %i[update], defaults: { format: :json }
 end
